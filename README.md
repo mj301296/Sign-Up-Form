5d5ac35b92c146ce8f9b3da7fb12e23a471cff3c
mj301296 <mrugankjadhav@gmail.com>
2023-06-13 19:24:10 -0700

    completed

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # Sign-Up-Form
 
+## Links
+
+- [Live App Here!](https://mj301296.github.io/Etch-A-Sketch/)
+
+- [Link to the Assignment](https://www.theodinproject.com/lessons/foundations-etch-a-sketch)
+
+## Built with
+
+- HTML
+- CSS
+- Javascript
+
+## About
+
+- A simple sign up page with password verification
+
+## Credits
+
+SVG:
 <a href="https://storyset.com/business">Business illustrations by Storyset</a>
 <a href="https://storyset.com/business">Business illustrations by Storyset</a>
 
--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const confirm = document.querySelector(`#confirm_password`);
+const submit = document.querySelector(`.submit_btn`);
 
 function matchPassword(){
     const password = document.querySelector(`#password`);
     console.log(password.value);
-    if(this.value !== password.value)
+    if(this.value !== password.value){
+        this.style.border = `solid`;
+        this.style.borderColor = `red`;
         this.classList.add(`focus`);
-    else
+        submit.disabled= `true`;
+    }
+    else{
+        console.log(`match`)
     this.classList.remove(`focus`);
+    this.style.border = `hidden`;
+    submit.removeAttribute("disabled");
+    }
 }
 
 confirm.addEventListener('input', matchPassword);
